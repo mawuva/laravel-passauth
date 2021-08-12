923c660963befc0aee536c5189edffafd5152e3f
Seddor Ephraim <seddorephraim7@gmail.com>
2021-08-12 14:23:18 +0100

    Update passauth.php config and create helpers.php

---
+++ config/passauth.php
@@ -0,0 +1,57 @@
+<?php
+
+/*
+ * You can place your custom package configuration in here.
+ */
+return [
+    /*
+    |--------------------------------------------------------------------------
+    | Users feature settings
+    |--------------------------------------------------------------------------
+    */
+
+    'user' => [
+        'model'             => App\Models\User::class,
+        'name'              => 'User',
+        'resource_name'     => 'user',
+
+        'table'     => [
+            'name'          => env('PASSAUTH_USERS_DATABASE_TABLE', 'users'),
+            'primary_key'   => env('PASSAUTH_USERS_DATABASE_TABLE_PRIMARY_KEY', 'id'),
+        ],
+    ],
+
+    /*
+    |--------------------------------------------------------------------------
+    | Required identifiants
+    |--------------------------------------------------------------------------
+    */
+
+    'required_identifiants' => [
+        'email'                 => true,
+        'phone_number'          => false,
+    ],
+
+    /*
+    |--------------------------------------------------------------------------
+    | Bunch of features to enable or disable.
+    |--------------------------------------------------------------------------
+    */
+
+    'enable' => [
+        'email_verification' => false,
+        'proper_names' => false,
+        'agree_with_policy_and_terms_data' => true,
+        'last_login_data' => true,
+    ],
+
+    'agree_with_policy_and_terms_column' => [
+        'name' => 'has_agreed_with_policy_and_terms_at',
+        'type' => 'timestamp'
+    ],
+
+    'last_login_column' => [
+        'name' => 'last_login_at',
+        'type' => 'timestamp'
+    ],
+];
--- src/PassauthServiceProvider.php
+++ src/PassauthServiceProvider.php
@@ -11,32 +11,34 @@ class PassauthServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+        require_once __DIR__ . '/helpers.php';
+
         /*
          * Optional methods to load your package assets
          */
-        // $this->loadTranslationsFrom(__DIR__.'/../resources/lang', 'laravel-passauth');
-        // $this->loadViewsFrom(__DIR__.'/../resources/views', 'laravel-passauth');
+        // $this->loadTranslationsFrom(__DIR__.'/../resources/lang', 'passauth');
+        // $this->loadViewsFrom(__DIR__.'/../resources/views', 'passauth');
         // $this->loadMigrationsFrom(__DIR__.'/../database/migrations');
         // $this->loadRoutesFrom(__DIR__.'/routes.php');
 
         if ($this->app->runningInConsole()) {
             $this->publishes([
-                __DIR__.'/../config/config.php' => config_path('laravel-passauth.php'),
+                __DIR__.'/../config/passauth.php' => config_path('passauth.php'),
             ], 'config');
 
             // Publishing the views.
             /*$this->publishes([
-                __DIR__.'/../resources/views' => resource_path('views/vendor/laravel-passauth'),
+                __DIR__.'/../resources/views' => resource_path('views/vendor/passauth'),
             ], 'views');*/
 
             // Publishing assets.
             /*$this->publishes([
-                __DIR__.'/../resources/assets' => public_path('vendor/laravel-passauth'),
+                __DIR__.'/../resources/assets' => public_path('vendor/passauth'),
             ], 'assets');*/
 
             // Publishing the translation files.
             /*$this->publishes([
-                __DIR__.'/../resources/lang' => resource_path('lang/vendor/laravel-passauth'),
+                __DIR__.'/../resources/lang' => resource_path('lang/vendor/passauth'),
             ], 'lang');*/
 
             // Registering package commands.
@@ -50,10 +52,10 @@ class PassauthServiceProvider extends ServiceProvider
     public function register()
     {
         // Automatically apply the package configuration
-        $this->mergeConfigFrom(__DIR__.'/../config/config.php', 'laravel-passauth');
+        $this->mergeConfigFrom(__DIR__.'/../config/config.php', 'passauth');
 
         // Register the main class to use with the facade
-        $this->app->singleton('laravel-passauth', function () {
+        $this->app->singleton('passauth', function () {
             return new Passauth;
         });
     }

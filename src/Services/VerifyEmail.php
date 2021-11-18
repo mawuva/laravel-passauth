b9c011be446b5c0d2340540c91f268e7440891e7
Seddor Ephraim <seddorephraim7@gmail.com>
2021-11-18 19:07:18 +0100

    Add VerifyEmail.php to implement user's email verification

---
+++ src/Services/VerifyEmail.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Mawuekom\Passauth\Services;
+
+use Exception;
+use Illuminate\Auth\Access\AuthorizationException;
+use Illuminate\Http\Response;
+
+class VerifyEmail
+{
+    /**
+     * Handle verify email.
+     *
+     * @param int|string $id
+     * @param string $hash
+     *
+     * @return array
+     */
+    public function __invoke($id, $hash): array
+    {
+        $user = get_user_by_id($id);
+
+        if (is_null($user)) {
+            throw new Exception(trans('passauth::messages.user_not_found'), Response::HTTP_NOT_FOUND);
+        }
+
+        if (! hash_equals((string) $hash, sha1($user ->getEmailForVerification()))) {
+            throw new AuthorizationException;
+        }
+
+        if (! $user ->hasVerifiedEmail()) {
+            $user ->markEmailAsVerified();
+        }
+
+        return success_response(trans('userly::messages.email_verified'));
+    }
+}

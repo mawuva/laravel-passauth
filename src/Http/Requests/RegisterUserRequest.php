853d44902952352de344fdb57976c6660953745a
Seddor Ephraim <seddorephraim7@gmail.com>
2021-11-18 17:36:54 +0100

    Update composer.json and RegisterUserRequest.php with toDTO method

---
+++ src/Http/Requests/RegisterUserRequest.php
@@ -0,0 +1,87 @@
+<?php
+
+namespace Mawuekom\Passauth\Http\Requests;
+
+use Illuminate\Validation\Rule;
+use Mawuekom\CustomUser\DataTransferObjects\StoreUserDTO;
+use Mawuekom\RequestCustomizer\FormRequestCustomizer;
+
+class RegisterUserRequest extends FormRequestCustomizer
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules(): array
+    {
+        $usersTable = config('custom-user.user.table.name');
+
+        $rules = [
+            'name'                          => [
+                'string',  Rule::unique($usersTable, 'name')
+            ],
+
+            'email'                         => [
+                'required', 'string', 'email', Rule::unique($usersTable, 'email')
+            ],
+            
+            'password'                      => 'string|min:6|confirmed',
+            'first_name'                    => 'string|nullable',
+            'phone_number'                  => 'string|nullable|regex:/^([0-9\s\-\+\(\)]*)$/',
+            'agree_with_policy_and_terms'   => 'string|nullable',
+        ];
+
+        (!get_attribute('name', 'optional'))
+            ?? array_push($rules['name'], 'required');
+
+        (get_attribute('phone_number', 'unique'))
+            ?? array_push($rules['phone_number'], Rule::unique($usersTable, 'phone_number'));
+
+        return $rules;
+    }
+
+    /**
+     * Get sanitizers defined for form input
+     *
+     * @return array
+     */
+    public function sanitizers(): array
+    {
+        return [
+            'name' => [
+                CapitalizeEachWords::class,
+            ],
+            'first_name' => [
+                CapitalizeEachWords::class,
+            ],
+        ];
+    }
+
+    /**
+     * Build and return a DTO
+     *
+     * @return \Mawuekom\CustomUser\DataTransferObjects\StoreUserDTO
+     */
+    public function toDTO(): StoreUserDTO
+    {
+        return new StoreUserDTO([
+            'name'                          => $this ->name,
+            'email'                         => $this ->email,
+            'password'                      => $this ->password,
+            'first_name'                    => $this ->first_name,
+            'phone_number'                  => $this ->phone_number,
+            'agree_with_policy_and_terms'   => $this ->agree_with_policy_and_terms,
+        ]);
+    }
+}
